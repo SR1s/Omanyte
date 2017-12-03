53355495831a0c51913bad36d2cd1f4a9e8630a5
sr1s <seemsnevermind@gmail.com>
2017-12-03 21:11:14 +0800

    app Application

---
+++ app/src/main/java/me/sr1/omanyte/OmanyteApp.java
@@ -0,0 +1,14 @@
+package me.sr1.omanyte;
+
+import android.app.Application;
+
+/**
+ *
+ * @author SR1
+ */
+
+public class OmanyteApp extends Application {
+
+    public static final String BASE_URL = "http://blah.me/";
+
+}
